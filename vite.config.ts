3fc9b73ad9274a21cb2c69e8755d5fb6e1e29606
Daniel Desira <desiradaniel2007@gmail.com>
2026-02-24 21:52:12 +0100

    Add https for local environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(
+        path.resolve(__dirname, "certs/localhost+2-key.pem"),
+      ),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost+2.pem")),
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
