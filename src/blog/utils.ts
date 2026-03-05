ccf51926d442a8231a894df8adfaba01b08706dc
Daniel Desira <desiradaniel2007@gmail.com>
2026-03-05 11:06:02 +0100

    #1: Refactor date formatter

--- src/blog/utils.ts
+++ src/blog/utils.ts
@@ -1,10 +1,15 @@
+const dateFormatter = new Intl.DateTimeFormat("en-GB", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
 export const formatDate = (input: string) => {
   const date = new Date(input);
-  return new Intl.DateTimeFormat("en-GB", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  }).format(date);
+  if (Number.isNaN(date.getTime())) {
+    return "Invalid date";
+  }
+  return dateFormatter.format(date);
 };
